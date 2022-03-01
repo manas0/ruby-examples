b487d08483e58b7538fd8ff51ece9a9bca3ba4ea
Manas Ghai <manasg46@gmail.com>
2022-03-01 13:26:18 +0530

    arrays

---
+++ arrays.rb
@@ -0,0 +1,14 @@
+days_of_week = Array.new(7, "day")
+print days_of_week.empty?
+print "\n"
+print days_of_week
+print "\n"
+
+array1 = ["Word", 1, 1.0, 3.4, true]
+array2 = ["V" , "Word", 1, 1.0, 3.4, true]
+num = [1,3,2,4,2,5]
+num.sort!
+print num
+
+print array1.eql? array2 
+print "\n"
