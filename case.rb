37f7ed940f523a47720800018da22343a09e007d
Manas Ghai <manasg46@gmail.com>
2022-03-01 15:30:05 +0530

    case statement

---
+++ case.rb
@@ -0,0 +1,11 @@
+score = 40
+
+result = case score
+   when 0..39 then "Fail"
+   when 0..60 then "Pass"
+   when 61..70 then "Pass with Merit"
+   when 71..100 then "Pass with Distinction"
+   else "Invalid Score"
+end
+
+puts result
