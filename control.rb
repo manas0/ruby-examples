5cb438e2798f8e086c1ed05ab85e561ec37d6234
Manas Ghai <manasg46@gmail.com>
2022-03-01 14:43:48 +0530

    if statement

---
+++ control.rb
@@ -0,0 +1,10 @@
+if 1<2 then 
+    print"1 is less than 2.\n" 
+end
+
+customerName = "John"
+
+if customerName == "Fred" then print "Hello Fred!"
+elsif customerName == "John" then print "Hello John!" 
+elsif customername == "Robert" then print "Hello Bob!"
+end
