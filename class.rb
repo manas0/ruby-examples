3090975f8be103b0c8a3c980caf563bd338a81ab
Manas Ghai <manasg46@gmail.com>
2022-03-02 18:21:42 +0530

    inheritance example

--- class.rb
+++ class.rb
@@ -4,7 +4,11 @@ class BankAccount
     def interest_rate
         @@interest_rate = 0.2
     end
-    
+
+    def calc_interest(balance)
+        puts balance*interest_rate
+    end
+
     def accountNumber
         @accountNymber
     end
@@ -36,3 +40,21 @@ account.accountName = "Peter"
 account.accountNumber = 343
 puts account.accountName
 puts account.accountNumber
+account.calc_interest(1000)
+
+class NewBankAccount < BankAccount
+    def customerPhone
+        @customerPhone
+    end
+
+    def customerPhone=(value)
+        @customerPhone = value
+    end
+end
+
+account2 = NewBankAccount.new()
+account2.accountName = "George"
+account2.accountNumber = 4323
+account2.customerPhone = 342892384
+
+puts account2.customerPhone
