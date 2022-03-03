e87aa534a7e7872fcadea1a219764c89e685888f
Manas Ghai <manasg46@gmail.com>
2022-03-03 12:31:07 +0530

    HereDocs

--- strings.rb
+++ strings.rb
@@ -2,8 +2,32 @@ s1 = String.new
 s2 = String.new()
 s3 = String.new("Hello")
 s4 = "Hi"
+s5 = String("Welcome")
 
 puts s1
 puts s2
 puts s3
 puts s4
+puts s5
+
+m1 = "Hi there.\nHow are you?"
+m2 = 'Hi there.\nHow are you?'
+
+puts m1
+puts m2
+
+myString = %'Hi "there"\nHow are you?'
+
+puts myString
+
+test = 'Hi "there\n"Hello'
+puts test
+
+text = <<DOC
+Hello
+    Hi
+        Hi "you"
+DOC
+
+
+puts text
